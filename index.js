cf16ea1a3325ac5abdb878bad0abe97e268e9b9c
Vitalii978 <Vitalii.Klymenko78@gmail.com>
2025-06-15 21:16:30 +0000

    Deploying to gh-pages from @ Vitalii978/goit-js-hw-11@1c857e641121a61dd098c574e018a3ba93edd368 🚀

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-import{a as d,S as f,i as a}from"./assets/vendor-1AYLTIiv.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();const p="50855680-e7e32480db9ff9b763cd16451",h="https://pixabay.com/api";function m(s){const o={key:p,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0};return d.get(h,{params:o}).then(e=>e.data).catch(e=>{throw console.error("Error fetching data:",e),e})}const l=document.querySelector(".gallery"),u=document.querySelector(".loader");let y=new f(".gallery a",{captionsData:"alt",captionDelay:250});function g(s){const o=s.map(e=>`
+import{a as f,i as p,S as m}from"./assets/vendor-vwbIfzmB.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const h="50855680-e7e32480db9ff9b763cd16451",y="https://pixabay.com/api/";function g(s){const r={key:h,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0};return f.get(y,{params:r}).then(L)}function L(s){const r=s.data.hits;return r.length===0?(p.warning({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",color:"red"}),[]):r}const c=document.querySelector(".gallery"),l=document.querySelector(".loader");document.querySelector(".form");let b=new m(".gallery a",{});function q(s){const r=s.map(({webformatURL:n,largeImageURL:i,tags:e,likes:t,views:o,comments:u,downloads:d})=>`
       <li class="gallery-item">
-        <a href="${e.largeImageURL}">
-          <img src="${e.webformatURL}" alt="${e.tags}" />
+        <a href="${i}">
+          <img src="${n}" alt="${e}" width='360'/>
         </a>
-        <div class="info">
-          <p><b>Likes:</b> ${e.likes}</p>
-          <p><b>Views:</b> ${e.views}</p>
-          <p><b>Comments:</b> ${e.comments}</p>
-          <p><b>Downloads:</b> ${e.downloads}</p>
-        </div>
-      </li>
-    `).join("");l.insertAdjacentHTML("beforeend",o),y.refresh()}function b(){l.innerHTML=""}function L(){u.classList.remove("is-hidden")}function w(){u.classList.add("is-hidden")}const c=document.querySelector(".form");c.addEventListener("submit",function(s){s.preventDefault();const e=c.elements["search-text"].value.trim();if(e===""){a.warning({message:"Please enter a search query!",position:"topRight"});return}b(),L(),m(e).then(n=>{n.hits.length===0?a.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):g(n.hits)}).catch(()=>{a.error({message:"Something went wrong. Please try again later.",position:"topRight"})}).finally(()=>{w()})});
+        <ul class='list'>
+      
+      <li class='item'>
+         <span>Likes</span>
+         <span>${t}</span>
+        </li>
+
+        <li class='item'>
+          <span>views</span>
+         <span>${o}</span>
+         </li>
+
+         <li class='item'>
+          <span>comments</span>
+         <span>${u}</span>
+         </li>
+
+         <li class='item'>
+          <span>downloads</span>
+         <span>${d}</span>
+         </li>
+       </ul>
+      </li>`).join("");c.insertAdjacentHTML("beforeend",r),b.refresh()}function S(){c.innerHTML=""}function v(){l.classList.remove("is-hidden")}function w(){l.classList.add("is-hidden")}const a=document.querySelector(".form");a.addEventListener("submit",function(s){s.preventDefault();const n=a.elements["search-text"].value.trim();if(n===""){a.reset();return}S(),v(),g(n).then(q).catch(console.log).finally(w),a.reset()});
 //# sourceMappingURL=index.js.map
